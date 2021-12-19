77963ba308027a9856639e3d502e0803b96283ee
wish911wish <wish911wish@gmail.com>
2021-12-19 17:59:31 +0900

    add story book

--- backend/.storybook/main.js
+++ backend/.storybook/main.js
@@ -1,3 +1,5 @@
+const path = require('path');
+
 module.exports = {
   stories: ['../stories/**/*.stories.mdx', '../stories/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
@@ -11,4 +13,11 @@ module.exports = {
       propFilter: (prop) => (prop.parent ? !/node_modules/.test(prop.parent.fileName) : true),
     },
   },
+  webpackFinal: async config => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': path.resolve(__dirname, '../resources/js')
+    }
+    return config;
+  }
 }
