0d9833f11d8449b3a548afb497483e9112552a8d
wish911wish <wish911wish@gmail.com>
2021-12-20 22:14:09 +0900

    fix form error

--- backend/resources/js/Components/Atoms/FormError.tsx
+++ backend/resources/js/Components/Atoms/FormError.tsx
@@ -1,11 +1,17 @@
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react'
 import * as React from 'react'
+
+const style = css({
+  color: 'red',
+})
 
 type Props = {
   text: string
 }
 
 const Label: React.FC<Props> = (props: Props) => {
-  return <p>{props.text}</p>
+  return <p css={style}>{props.text}</p>
 }
 
 export default Label
