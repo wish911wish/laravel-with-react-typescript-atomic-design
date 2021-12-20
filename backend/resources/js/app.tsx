15e84f87030c0cc44984f7af0e0166ec39ece679
wish911wish <wish911wish@gmail.com>
2021-12-20 23:27:46 +0900

    fix app.tsx

--- backend/resources/js/app.tsx
+++ backend/resources/js/app.tsx
@@ -1,11 +1,10 @@
-import React from 'react'
 import { render } from 'react-dom'
 import { createInertiaApp } from '@inertiajs/inertia-react'
 import { InertiaProgress } from '@inertiajs/progress'
 
 createInertiaApp({
   resolve: (name) => require(`./Pages/${name}`),
-  setup({ el, App, props }) {
+  setup: ({ el, App, props }) => {
     render(<App {...props} />, el)
   },
 })
